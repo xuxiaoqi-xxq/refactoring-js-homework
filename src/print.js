8fb96c91061b21fd5fcb04fbf239039e957b8ef2
xxq <xxq_work@163.com>
2020-08-28 23:43:47 +0800

    [eva] feature: change console log to return value

--- src/print.js
+++ src/print.js
@@ -1,8 +1,8 @@
 function printOwing (invoice) {
   let outstanding = 0;
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+  let result = '***********************\n' +
+      '**** Customer Owes ****\n' +
+      '***********************\n';
 
   // calculate outstanding
   for (const o of invoice.borderSpacing) {
@@ -14,7 +14,12 @@ function printOwing (invoice) {
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
   // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+  result += `name: ${invoice.customer}\n`;
+  result += `amount: ${outstanding}\n`;
+  result += `amount: ${invoice.dueDate.toLocaleDateString()}\n`;
+  return result;
+}
+
+module.exports = {
+  printOwing
 }
