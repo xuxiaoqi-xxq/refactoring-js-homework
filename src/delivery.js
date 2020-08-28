df89623563a3d47ee2607c4bfe0afdbadd965817
xxq <xxq_work@163.com>
2020-08-29 00:26:35 +0800

    [eva] refactor: extract calculate delivery time statement when isRush is false into calculateDeliveryTime method

--- src/delivery.js
+++ src/delivery.js
@@ -1,44 +1,46 @@
-function calculateDeliveryTime(deliveryState) {
+function calculateDeliveryTime(deliveryState, isRush) {
   let deliveryTime;
-  if ([
-    'MA',
-    'CT',
-  ].includes(deliveryState)) {
-    deliveryTime = 1;
-  } else if ([
-    'NY',
-    'NH',
-  ].includes(deliveryState)) {
-    deliveryTime = 2;
-  } else {
-    deliveryTime = 3;
-  }
-  return deliveryTime;
-}
-
-function deliveryDate (anOrder, isRush) {
-  if (isRush) {
-    let deliveryTime = calculateDeliveryTime(anOrder.deliveryState);
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
-  }
-  else {
-    let deliveryTime;
+  if(isRush){
+    if ([
+      'MA',
+      'CT',
+    ].includes(deliveryState)) {
+      deliveryTime = 1;
+    } else if ([
+      'NY',
+      'NH',
+    ].includes(deliveryState)) {
+      deliveryTime = 2;
+    } else {
+      deliveryTime = 3;
+    }
+  }else {
     if ([
       'MA',
       'CT',
       'NY',
-    ].includes(anOrder.deliveryState)) {
+    ].includes(deliveryState)) {
       deliveryTime = 2;
     }
     else if ([
       'ME',
       'NH',
-    ].includes(anOrder.deliveryState)) {
+    ].includes(deliveryState)) {
       deliveryTime = 3;
     }
     else {
       deliveryTime = 4;
     }
+  }
+  return deliveryTime;
+}
+
+function deliveryDate (anOrder, isRush) {
+  let deliveryTime = calculateDeliveryTime(anOrder.deliveryState, isRush);
+  if (isRush) {
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+  }
+  else {
     return anOrder.placedOn.plusDays(2 + deliveryTime);
   }
 }
